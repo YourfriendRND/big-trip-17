1bfacbe6751a9b8ed9b0dbf2a13b558023c56c54
Anton <kupeirc@gmail.com>
2022-04-29 18:45:40 +0300

    Добавил view компоненты и trip presenter

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import EventFilterForm from "./view/filters-view";
+import TripInfo from "./view/trip-info-view";
+import TripMapPresenter from "./presenter/trip-map-presenter";
+import { render } from "./render";
+
+const tripMainBlock = document.querySelector(".trip-main"); 
+const filterBlock = document.querySelector(".trip-controls__filters");
+const tripEventsBlock = document.querySelector(".trip-events");
+
+const tripMapPresenter = new TripMapPresenter(); 
+
+render(new TripInfo(), tripMainBlock, "afterbegin");
+render(new EventFilterForm(), filterBlock);
+
+tripMapPresenter.init(tripEventsBlock)
---
+++ src/presenter/trip-map-presenter.js
@@ -0,0 +1,27 @@
+import Destination from "../view/destination-view";
+import EditEventForm from "../view/edit-event-form-view";
+import EventItem from "../view/event-item-view";
+import NewEventForm from "../view/new-event-form-view";
+import EventSortForm from "../view/sort-view";
+import EventList from "../view/event-list-view";
+import Offers from "../view/offers-view";
+import { render } from "../render";
+
+export default class TripMapPresenter {
+    init = (mapContainer) => {
+        this.mapContainer = mapContainer;
+        this.eventList = new EventList(); 
+        this.newEventForm = new NewEventForm();
+        this.editEventForm = new EditEventForm(); 
+
+        render(new EventSortForm(), this.mapContainer);
+        render(this.eventList, this.mapContainer); 
+        render(this.newEventForm, this.eventList.getElement());
+        render(new Offers(), this.newEventForm.getElement().querySelector(".event__details"));
+        render(new Destination(), this.newEventForm.getElement().querySelector(".event__details"));
+        Array.from({length: 3}, () => new EventItem()).forEach(component => render(component, this.eventList.getElement()));
+        render(this.editEventForm, this.eventList.getElement());
+        render(new Offers(), this.editEventForm.getElement().querySelector(".event__details"));
+        render(new Destination(), this.editEventForm.getElement().querySelector(".event__details"));        
+    }
+}
