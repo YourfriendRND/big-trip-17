46c685dc569291baae33d21da3ad9f07429e8152
YourfriendRND <kupeirc@gmail.com>
2022-06-21 13:29:59 +0300

    Внес правки в доп.функционал

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -2,27 +2,30 @@
 import AbstractView from '../framework/view/abstract-view';
 import { getFullRoute, getRoutePeriod } from '../util';
 
-const createTripInfoTemplate = (events) => `<section class="trip-main__trip-info  trip-info">
+const createTripInfoTemplate = (events, totalPrice) => `<section class="trip-main__trip-info  trip-info">
     <div class="trip-info__main">
       <h1 class="trip-info__title">${getFullRoute(events)}</h1>
       <p class="trip-info__dates">${getRoutePeriod(events)}</p>
     </div>
     <p class="trip-info__cost">
-      Total: &euro;&nbsp;<span class="trip-info__cost-value">${events.reduce((amount, event) => amount + event.basePrice + event.totalOfferPrice, 0)}</span>
+      Total: &euro;&nbsp;<span class="trip-info__cost-value">${totalPrice}</span>
     </p>
   </section>`;
 
 export default class TripInfoView extends AbstractView {
   #events = null;
   #offers = null;
+  #totalTripPrice = 0;
+
   constructor (events, offers) {
     super();
     this.#offers = [...offers];
-    this.#events = events.map((event) =>({...event, totalOfferPrice: this.#getEventOffersPrice(event)}));
+    this.#events = [...events];
+    this.#totalTripPrice = this.#events.reduce((amount, event) => amount + event.basePrice + this.#getEventOffersPrice(event), 0);
   }
 
   get template() {
-    return createTripInfoTemplate(this.#events);
+    return createTripInfoTemplate(this.#events, this.#totalTripPrice);
   }
 
   #getEventOffersPrice = (event) => {
